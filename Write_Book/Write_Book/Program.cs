eadb2af25b01b42a0b3da4ff5907dc7644b3b2e7
Andrey <andrey.gorelov@skillbox.ru>
2023-09-09 18:40:17 +0300

    Задание 4

---
+++ Write_Book/Write_Book/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Write_Book
+{
+    internal class Program
+    {
+
+        public static void AddWorkerToXMLFile()
+        {
+            Console.Write("Введите ФИО: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Введите улицу: ");
+            string street = Console.ReadLine();
+
+            Console.Write("Введите номер дома: ");
+            string houseNumber = Console.ReadLine();
+
+            Console.Write("Введите номер квартиры: ");
+            string flatNumber = Console.ReadLine();
+
+            Console.Write("Введите мобильный телефон: ");
+            string mobilePhone = Console.ReadLine();
+
+            Console.Write("Введите домашний телефон: ");
+            string homePhone = Console.ReadLine();
+
+            XElement xml = new XElement("Person",
+               new XAttribute("name", name),
+               new XElement("Address",
+               new XElement("Street", street),
+               new XElement("HouseNumber", houseNumber),
+               new XElement("FlatNumber", flatNumber)
+           ),
+               new XElement("Phones",
+               new XElement("MobilePhone", mobilePhone),
+               new XElement("HomePhone", homePhone)
+           )
+       );
+
+            xml.Save("contacts.xml");
+        }
+
+        static void Main(string[] args)
+        {
+            AddWorkerToXMLFile();
+        }
+    }
+}
